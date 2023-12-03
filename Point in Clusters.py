eb507f745d58cdcae4357be952115ef7dbf8165c
Harsha S <67546267+harsha-art@users.noreply.github.com>
2023-12-03 14:24:18 +0530

    Changes to Graph

--- Point in Clusters.py
+++ Point in Clusters.py
@@ -6,14 +6,14 @@ import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-dataframe = pd.read_csv("abnormal.csv",low_memory=False)
+dataframe = pd.read_csv(r"C:\Users\shars\Downloads\abnormal.csv",low_memory=False)
 #print(dataframe.shape)
 
 
 app.layout = dbc.Container([
 
     dbc.Row(
-        dbc.Col(html.H1("Machine Learning Stuff Idk Tbh ",
+        dbc.Col(html.H1("Enter The X Axis Column Here",
                         className='text-center text-white mb-4'),
                 width=12)
     ),
@@ -54,21 +54,12 @@ app.layout = dbc.Container([
 def update_text(chosen_val):
     print(chosen_val)
     if chosen_val == "":
-        fig = px.scatter(dataframe, x='Age', y='BCLOT',size='Age',color='BCLOT',
+        fig = px.scatter(dataframe, x='SGOT', y='BCLOT',color='BCLOT',
                          hover_name=dataframe.index,size_max=65)
     else:
-        input_list = chosen_val.split(",")
-        print(input_list)
-        val = {
-            "Age": [input_list[0]], "Gender": [input_list[1]], "BCLOT": [input_list[2]], "A / G": [input_list[3]],
-            "RAT": [input_list[4]], "DBIL": [input_list[5]], 'IBIL': [input_list[6]], 'TBIL': [input_list[7]],
-            "TREP": [input_list[8]], "ALB": [input_list[9]], "GGT": [input_list[10]], "SGOT": [input_list[11]],
-            "SGPT": [input_list[12]], "ALKP04": [input_list[13]]
-        }
-        df1 = pd.DataFrame(val)
-        fig = px.scatter(dataframe, x='Age', y='BCLOT',size='BCLOT',color='Age',
+        fig = px.scatter()
+        fig = px.scatter(dataframe, x=chosen_val, y='BCLOT',color='Age',
                          hover_name=dataframe.index,size_max=65)
-        fig.add_trace(go.Scatter(x=df1["Age"], y=df1["BCLOT"], marker_symbol='x', marker_size=100 ))
 
     return fig
 
